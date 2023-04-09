79871488197e53c52d4a300cbf033c6db9442330
fernandomema <fernandomema@gmail.com>
2023-04-09 20:23:28 +0200

    Fixes

--- svelte.config.js
+++ svelte.config.js
@@ -5,9 +5,6 @@ import adapter from "@sveltejs/adapter-static";
 const config = {
   kit: {
     adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === "production" ? "/sveltekit-gh-pages" : "",
-    },
   },
 
   preprocess: [
